91a1732d967c2f6694707f2928acd3ac2aa81b39
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-19 14:38:52 +0700

    Biodata Keluarga

---
+++ src/biodata/Biodata.java
@@ -0,0 +1,107 @@
+
+package biodata;
+
+
+public class Biodata {
+
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+    private String namaayah;
+    private String namaibu;
+    private String namasaudara;
+    private String namamu;
+    private String alamat;
+    private String tempatlahir;
+    private String tanggallahir;
+    private String hobi;
+    private String citacita;
+    private String nomertelepon;
+
+
+    public void setNamaayah(String namaayah) {
+        this.namaayah = namaayah;
+    }
+
+    public void setNamaibu(String namaibu) {
+        this.namaibu = namaibu;
+    }
+
+    public void setNamasaudara(String namasaudara) {
+        this.namasaudara = namasaudara;
+    }
+
+    public void setNamamu(String namamu) {
+        this.namamu = namamu;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setTempatlahir(String tempatlahir) {
+        this.tempatlahir = tempatlahir;
+    }
+
+    public void setTanggallahir(String tanggallahir) {
+        this.tanggallahir = tanggallahir;
+    }
+
+    public void setHobi(String hobi) {
+        this.hobi = hobi;
+    }
+
+    public void setCitacita(String citacita) {
+        this.citacita = citacita;
+    }
+
+    public void setNomertelepon(String nomertelepon) {
+        this.nomertelepon = nomertelepon;
+    }
+
+    public String getNamaayah() {
+        return namaayah;
+    }
+
+    public String getNamaibu() {
+        return namaibu;
+    }
+
+    public String getNamasaudara() {
+        return namasaudara;
+    }
+
+    public String getNamamu() {
+        return namamu;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getTempatlahir() {
+        return tempatlahir;
+    }
+
+    public String getTanggallahir() {
+        return tanggallahir;
+    }
+
+    public String getHobi() {
+        return hobi;
+    }
+
+    public String getCitacita() {
+        return citacita;
+    }
+
+    public String getNomertelepon() {
+        return nomertelepon;
+    }
+}
+
+
+ 
+    
---
+++ src/biodata/Biodata2.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Biodata2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Biodata biodata = new Biodata();
+        biodata.setNamaayah("Nurbagyo");
+        biodata.setNamaibu("IdaRirin");
+        biodata.setNamasaudara("Adila Cantya Danastri");
+        biodata.setNamamu("Caesario Fathan Nugroho");
+        biodata.setAlamat("JL.jayasrani 7c/24");
+        biodata.setTempatlahir("Malang");
+        biodata.setTanggallahir("14 September 2002");
+        biodata.setHobi("Lari");
+        biodata.setCitacita("Tentara");
+        biodata.setNomertelepon("082141278883");
+        
+        System.out.println("Nama ayah: " + biodata.getNamaayah());
+        System.out.println("Nama ibu:" + biodata.getNamaibu());
+        System.out.println("Nama saudara: " + biodata.getNamasaudara()  );
+        System.out.println("Namamu: " + biodata.getNamamu());
+        System.out.println("Alamat: " + biodata.getAlamat());
+        System.out.println("Tempat lahir: " + biodata.getTempatlahir());
+        System.out.println("Tanggal lahir: " + biodata.getTanggallahir());
+        System.out.println("Hobi: " + biodata.getHobi());
+        System.out.println("Cita cita: " + biodata.getCitacita());
+        System.out.println("Nomer telepon: " + biodata.getNomertelepon());
+                
+                
+    }
+    
+}
+
+
+      
+        
+        
+    
+    
+
